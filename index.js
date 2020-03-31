f6fae4827593f07e572a850a13be14213e90d387
Filip Juric <wizehood@gmail.com>
2020-03-31 20:28:43 +0200

    introduced eslint + rules

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const Scraper = require('./utils/scraper');
 const Parser = require('./parser');
 
 const main = async () => {
-    try {
-        const io = new Io();
-        await io.init();
+	try {
+		const io = new Io();
+		await io.init();
 
-        const parser = new Parser();
-        const monitor = new Monitor();
-        const scraper = new Scraper(parser, io, monitor);
+		const parser = new Parser();
+		const monitor = new Monitor();
+		const scraper = new Scraper(parser, io, monitor);
 
-        await scraper.init();
-        await scraper.run();
-    }
-    catch (err) {
-        console.log(`Error catched: ${err}\nStack: ${err.stack}`);
-    }
-}
+		await scraper.init();
+		await scraper.run();
+	}
+	catch (err) {
+		console.log(`Error catched: ${err}\nStack: ${err.stack}`);
+	}
+};
 
 main();
